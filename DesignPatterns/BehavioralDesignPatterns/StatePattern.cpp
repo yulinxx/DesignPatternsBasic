54763937a290fdda597c83dcf7d0208bc9616e57
xx <xx@ubuntu.com>
2022-06-26 17:23:51 +0800

    状态模式

--- DesignPatterns/BehavioralDesignPatterns/StatePattern.cpp
+++ DesignPatterns/BehavioralDesignPatterns/StatePattern.cpp
@@ -1,8 +1,20 @@
+// 状态模式(state pattern)
+
+// 状态模式：允许一个对象在其内部状态改变时改变它的行为。对象看起来似乎修改了它的类。
+// 它有两种使用情况：
+// （1）一个对象的行为取决于它的状态, 并且它必须在运行时刻根据状态改变它的行为。
+// （2）一个操作中含有庞大的多分支的条件语句，且这些分支依赖于该对象的状态。
+// 本文的例子为第一种情况，以战争为例，假设一场战争需经历四个阶段：前期、中期、后期、结束。
+// 当战争处于不同的阶段，战争的行为是不一样的，也就说战争的行为取决于所处的阶段，而且随着时间的推进是动态变化的。
+// https://blog.csdn.net/pomelozero/article/details/76713677
+
+
 #include <iostream>
 using namespace std;
 
 class War;
 
+///////////////////////////////////////////////////
 class State
 {
 public:
@@ -13,13 +25,11 @@ public:
 	virtual void CurrentState(War *war) {} // 当前状态
 };
 
+
+///////////////////////////////////////////////////
 // 战争
 class War
 {
-private:
-	State *m_pState;  //目前状态
-	int m_nDays;      //战争持续时间
-
 public:
 	War(State *state) : m_pState(state), m_nDays(0) {}
 	~War() { delete m_pState; }
@@ -28,11 +38,16 @@ public:
 	void SetDays(int days) { m_nDays = days; }
 	void SetState(State *state) { delete m_pState; m_pState = state; }
 	void GetState() { m_pState->CurrentState(this); }
+
+private:
+	State *m_pState = nullptr;  //目前状态
+	int m_nDays;      //战争持续时间
 };
 
 
 // 给出具体的状态类：
 
+///////////////////////////////////////////////////
 // 战争结束
 class EndState : public State
 {
@@ -44,6 +59,7 @@ public:
 	void CurrentState(War *war) { End(war); }
 };
 
+/////////////////////////////////////
 // 后期
 class AnaphaseState : public State
 {
@@ -61,6 +77,7 @@ public:
 	void CurrentState(War *war) { Anaphase(war); }
 };
 
+///////////////////////////////////////////////////
 // 中期
 class MetaphaseState : public State
 {
@@ -79,6 +96,7 @@ public:
 	void CurrentState(War *war) { Metaphase(war); }
 };
 
+///////////////////////////////////////////////////
 // 前期
 class ProphaseState : public State
 {
@@ -97,7 +115,7 @@ public:
 	void CurrentState(War *war) { Prophase(war); }
 };
 
-/////////////////////////////////////
+///////////////////////////////////////////////////
 // 测试案例
 int main()
 {
