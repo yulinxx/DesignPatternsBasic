a33b22718d31410bf25e4b3f47efccae453ab33b
xx <xx@ubuntu.com>
2022-07-03 20:57:12 +0800

    State

--- DesignPatterns/BehavioralDesignPatterns/StatePattern.cpp
+++ DesignPatterns/BehavioralDesignPatterns/StatePattern.cpp
@@ -8,7 +8,6 @@
 // 当战争处于不同的阶段，战争的行为是不一样的，也就说战争的行为取决于所处的阶段，而且随着时间的推进是动态变化的。
 // https://blog.csdn.net/pomelozero/article/details/76713677
 
-
 #include <iostream>
 using namespace std;
 
@@ -18,13 +17,12 @@ class War;
 class State
 {
 public:
-	virtual void Prophase() {}	// 前期
-	virtual void Metaphase() {}	// 中期
-	virtual void Anaphase() {}	// 后期
-	virtual void End() {}			// 结束
+	virtual void Prophase() {}			   // 前期
+	virtual void Metaphase() {}			   // 中期
+	virtual void Anaphase() {}			   // 后期
+	virtual void End() {}				   // 结束
 	virtual void CurrentState(War *war) {} // 当前状态
 };
-
 
 ///////////////////////////////////////////////////
 // 战争
@@ -36,14 +34,17 @@ public:
 
 	int GetDays() { return m_nDays; }
 	void SetDays(int days) { m_nDays = days; }
-	void SetState(State *state) { delete m_pState; m_pState = state; }
+	void SetState(State *state)
+	{
+		delete m_pState;
+		m_pState = state;
+	}
 	void GetState() { m_pState->CurrentState(this); }
 
 private:
-	State *m_pState = nullptr;  //目前状态
-	int m_nDays;      //战争持续时间
+	State *m_pState = nullptr; // 目前状态
+	int m_nDays;			   // 战争持续时间
 };
-
 
 // 给出具体的状态类：
 
@@ -52,7 +53,7 @@ private:
 class EndState : public State
 {
 public:
-	void End(War *war) //结束阶段的具体行为
+	void End(War *war) // 结束阶段的具体行为
 	{
 		cout << "战争结束" << endl;
 	}
@@ -64,9 +65,9 @@ public:
 class AnaphaseState : public State
 {
 public:
-	void Anaphase(War *war) //后期的具体行为
+	void Anaphase(War *war) // 后期的具体行为
 	{
-		if (war->GetDays() < 30) 
+		if (war->GetDays() < 30)
 			cout << "第" << war->GetDays() << "天：战争后期，双方拼死一搏" << endl;
 		else
 		{
@@ -82,11 +83,10 @@ public:
 class MetaphaseState : public State
 {
 public:
-	void Metaphase(War *war) //中期的具体行为
+	void Metaphase(War *war) // 中期的具体行为
 	{
 		if (war->GetDays() < 20)
-			cout << "第" << war->GetDays() << 
-					"天：战争中期，进入相持阶段，双发各有损耗" << endl;
+			cout << "第" << war->GetDays() << "天：战争中期，进入相持阶段，双发各有损耗" << endl;
 		else
 		{
 			war->SetState(new AnaphaseState());
@@ -101,11 +101,10 @@ public:
 class ProphaseState : public State
 {
 public:
-	void Prophase(War *war)  //前期的具体行为
+	void Prophase(War *war) // 前期的具体行为
 	{
 		if (war->GetDays() < 10)
-			cout << "第" << war->GetDays() <<
-				"天：战争初期，双方你来我往，互相试探对方" << endl;
+			cout << "第" << war->GetDays() << "天：战争初期，双方你来我往，互相试探对方" << endl;
 		else
 		{
 			war->SetState(new MetaphaseState());
